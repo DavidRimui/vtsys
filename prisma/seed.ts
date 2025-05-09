04d8553276963fa409d4b3afc848ef8ae79f6fc7
davidrimui <rimuikaranja>
2025-05-09 12:21:39 +0300

    Fix all Prisma client imports to use @prisma/client

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '.prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { hashPassword } from '../lib/auth-utils';
 
 const prisma = new PrismaClient();
