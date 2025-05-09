9731d8355cf01b73f66cc139162161e127874f37
davidrimui <rimuikaranja>
2025-05-09 12:08:13 +0300

    Fix Prisma client import in prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '.prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 // Use a singleton pattern to avoid multiple Prisma Client instances
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
