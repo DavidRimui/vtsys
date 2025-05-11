0b93268c768a4b6c7055d86e26661469db88ca9b
davidrimui <rimuikaranja>
2025-05-11 11:18:03 +0300

    Remove auth-related pages (admin, login, signup) to fix build

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from "@/components/auth/signup-form"
-
-export default function SignupPage() {
-  return (
-    <div className="container mx-auto px-4 py-16">
-      <SignupForm />
-    </div>
-  )
-}
